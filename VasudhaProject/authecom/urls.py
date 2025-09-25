24c61de7f03772fd3dca64fd1e10f8cc1afec678
Priyanka Mittha <mitthapriyanka16@gmail.com>
2025-09-25 16:48:16 +0530

    Login Page Done customer & Admin

--- VasudhaProject/authecom/urls.py
+++ VasudhaProject/authecom/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('all_logins/', views.all_logins,name="all_logins"),
     path('signup/', views.signup,name="signup"),
     path('login/', views.handlelogin,name="handlelogin"),
-    path('logout/', views.handlelogout,name="handlelogout")
+    path('logout/', views.handlelogout,name="handlelogout"),
+    path('login/customer/', views.login_customer, name='login_customer'),
+    path('login/admin/', views.login_admin, name='login_admin'),
+    path('signup/customer/', views.signup_customer, name='signup_customer'),
+    path('signup/admin/', views.signup_admin, name='signup_admin'),
 
 ]
--- VasudhaProject/authecom/views.py
+++ VasudhaProject/authecom/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def signup(request):
@@ -12,5 +12,24 @@ def all_logins(request):
 
 def handlelogout(request):
     return redirect('/authecom/login')
+
+def signup_customer(request):
+    # Logic for customer signup
+    return HttpResponse("signup customer")
+    
+
+def signup_admin(request):
+    # Logic for admin signup
+    return HttpResponse("signup admin")
+
+def login_customer(request):
+    # Logic for customer signup
+    pass
+
+def login_admin(request):
+    # Logic for admin signup
+    pass
+
+
 
 
